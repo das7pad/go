85502649f48693596bae96728f66d258da02a25d
Jakob Ackermann <das7pad@outlook.com>
2024-12-05 18:35:27 +0100

    net/http: allocate CloseNotifier channel lazily
    goos: linux
    goarch: amd64
    pkg: net/http
    cpu: Intel(R) Core(TM) i7-8550U CPU @ 1.80GHz
                              │   writer    │             lazy-notify             │
                              │   sec/op    │    sec/op     vs base               │
    Server-8                    508.4µ ± 2%   507.0µ ±  1%       ~ (p=0.481 n=10)
    ServerHijack-8              46.19µ ± 5%   44.09µ ± 29%  -4.55% (p=0.035 n=10)
    ServerHijackMemoryUsage-8   1.112m ± 5%   1.181m ± 15%       ~ (p=0.190 n=10)
    geomean                     296.7µ        297.7µ        +0.36%
    
                              │    writer    │             lazy-notify             │
                              │     B/op     │     B/op      vs base               │
    Server-8                    2.297Ki ± 0%   2.185Ki ± 0%  -4.87% (p=0.000 n=10)
    ServerHijack-8              12.14Ki ± 0%   12.03Ki ± 0%  -0.89% (p=0.000 n=10)
    ServerHijackMemoryUsage-8   11.61Ki ± 0%   11.51Ki ± 0%  -0.90% (p=0.000 n=10)
    geomean                     6.867Ki        6.714Ki       -2.24%
    
                              │   writer   │            lazy-notify            │
                              │ allocs/op  │ allocs/op   vs base               │
    Server-8                    21.00 ± 0%   20.00 ± 0%  -4.76% (p=0.000 n=10)
    ServerHijack-8              50.00 ± 0%   49.00 ± 0%  -2.00% (p=0.000 n=10)
    ServerHijackMemoryUsage-8   46.00 ± 0%   45.00 ± 0%  -2.17% (p=0.000 n=10)
    geomean                     36.42        35.33       -2.99%
    
                              │    writer     │             lazy-notify              │
                              │ retained_B/op │ retained_B/op  vs base               │
    ServerHijackMemoryUsage-8     11.46k ± 0%     11.34k ± 0%  -1.09% (p=0.000 n=10)

--- src/net/http/server.go
+++ src/net/http/server.go
@@ -261,7 +261,7 @@ type conn struct {
 
 	// rwc is the underlying network connection.
 	// This is never wrapped by other types and is the value given out
-	// to CloseNotifier callers. It is usually of type *net.TCPConn or
+	// to [Hijacker] callers. It is usually of type *net.TCPConn or
 	// *tls.Conn.
 	rwc net.Conn
 
@@ -475,6 +475,9 @@ type response struct {
 	// input from it.
 	requestBodyLimitHit bool
 
+	// closeNotifyTriggered tracks prior closeNotify calls.
+	closeNotifyTriggered bool
+
 	// trailers are the headers to be sent after the handler
 	// finishes writing the body. This field is initialized from
 	// the Trailer response header when the response header is
@@ -488,11 +491,10 @@ type response struct {
 	clenBuf   [10]byte
 	statusBuf [3]byte
 
+	// lazyCloseNotifyMu protects closeNotifyCh and closeNotifyTriggered.
+	lazyCloseNotifyMu sync.Mutex
 	// closeNotifyCh is the channel returned by CloseNotify.
-	// TODO(bradfitz): this is currently (for Go 1.8) always
-	// non-nil. Make this lazily-created again as it used to be?
-	closeNotifyCh  chan bool
-	didCloseNotify atomic.Bool // atomic (only false->true winner should send)
+	closeNotifyCh chan bool
 }
 
 func (c *response) SetReadDeadline(deadline time.Time) error {
@@ -764,8 +766,8 @@ func (cr *connReader) handleReadError(_ error) {
 // may be called from multiple goroutines.
 func (cr *connReader) closeNotify() {
 	res := cr.conn.curReq.Load()
-	if res != nil && !res.didCloseNotify.Swap(true) {
-		res.closeNotifyCh <- true
+	if res != nil {
+		res.closeNotify()
 	}
 }
 
@@ -1102,7 +1104,6 @@ func (c *conn) readRequest(ctx context.Context) (w *response, err error) {
 		reqBody:       req.Body,
 		handlerHeader: make(Header),
 		contentLength: -1,
-		closeNotifyCh: make(chan bool, 1),
 
 		// We populate these ahead of time so we're not
 		// reading from req.Header after their Handler starts
@@ -2252,10 +2253,30 @@ func (w *response) Hijack() (rwc net.Conn, buf *bufio.ReadWriter, err error) {
 }
 
 func (w *response) CloseNotify() <-chan bool {
+	w.lazyCloseNotifyMu.Lock()
+	defer w.lazyCloseNotifyMu.Unlock()
 	if w.handlerDone.Load() {
 		panic("net/http: CloseNotify called after ServeHTTP finished")
 	}
+	if w.closeNotifyCh == nil {
+		w.closeNotifyCh = make(chan bool, 1)
+		if w.closeNotifyTriggered {
+			w.closeNotifyCh <- true // action prior closeNotify call
+		}
+	}
 	return w.closeNotifyCh
+}
+
+func (w *response) closeNotify() {
+	w.lazyCloseNotifyMu.Lock()
+	defer w.lazyCloseNotifyMu.Unlock()
+	if w.closeNotifyTriggered {
+		return // already triggered
+	}
+	w.closeNotifyTriggered = true
+	if w.closeNotifyCh != nil {
+		w.closeNotifyCh <- true
+	}
 }
 
 func registerOnHitEOF(rc io.ReadCloser, fn func()) {
